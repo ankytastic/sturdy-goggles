d87f7902759173c35c12a307f88a28e0e2fa2288
ANKIT RAJ <109132971+ankytastic@users.noreply.github.com>
2024-07-09 10:22:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1701-average-waiting-time/1701-average-waiting-time.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    double averageWaitingTime(vector<vector<int>>& customers) {
+        double curT=0;
+        double waitT=0;
+        sort(customers.begin(),customers.end());
+        for(int i=0;i<customers.size();i++){
+            if(i==0){
+                curT+=customers[i][0]+customers[i][1];
+                waitT+=customers[i][1];
+            }else{
+                if(curT>customers[i][0]){
+                    waitT+=curT-customers[i][0];
+                }else if(curT<customers[i][0]){
+                    curT=customers[i][0];
+                }
+                curT+=customers[i][1];
+                waitT+=customers[i][1];
+            }
+        }
+        return waitT/customers.size();
+    }
+};
