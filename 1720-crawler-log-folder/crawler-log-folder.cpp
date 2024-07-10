aef7619a4d4ee07b39c188c66d90f1377afdd13f
ANKIT RAJ <109132971+ankytastic@users.noreply.github.com>
2024-07-10 08:05:01 +0530

    Time: 4 ms (67.13%) | Memory: 14 MB (61.11%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int depth=0;
+        for(auto log: logs){
+            if(log=="../"){
+                if(depth>0) depth--;
+            }
+            else if(log!="./"){
+                depth++;
+            }
+        }
+        return depth;
+    }
+};
