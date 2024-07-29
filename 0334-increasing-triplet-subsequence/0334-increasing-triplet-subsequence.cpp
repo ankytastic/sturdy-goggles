799ccca2e48ed652d4d47b5e8f23cfbf3d9c346f
ANKIT RAJ <109132971+ankytastic@users.noreply.github.com>
2024-07-29 12:27:01 +0530

    Time: 75 ms (88.56%), Space: 114.4 MB (32.7%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int n1=INT_MAX;
+        int n2=INT_MAX;
+        for(auto n:nums){
+            if(n<=n1){
+                n1=n;
+            }
+            else if(n<=n2){
+                n2= n;
+            }else return true;
+        }
+        return false;
+    }
+};
