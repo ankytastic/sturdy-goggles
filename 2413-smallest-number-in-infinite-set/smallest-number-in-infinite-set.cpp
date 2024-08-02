8fc278421e3fa1dc0753c8a332612c1b59fa1390
ANKIT RAJ <109132971+ankytastic@users.noreply.github.com>
2024-08-02 14:18:01 +0530

    Time: 68 ms (59.04%) | Memory: 40.4 MB (76.91%) - LeetSync

---
+++ 2413-smallest-number-in-infinite-set/smallest-number-in-infinite-set.cpp
@@ -0,0 +1,30 @@
+class SmallestInfiniteSet {
+public:
+    int res;
+    unordered_set<int> s;
+    priority_queue<int,vector<int>,greater<int>> qu;
+    SmallestInfiniteSet() {
+        res=1;
+    }
+    
+    int popSmallest() {
+        int r;
+        if(!qu.empty()){
+            r=qu.top();
+            qu.pop();
+            s.erase(r);
+        }else{
+            r=res;
+            res+=1;
+        }
+        return r;
+    }
+    
+    void addBack(int num) {
+        if(num>=res || s.find(num)!=s.end()){
+            return;
+        }
+        s.insert(num);
+        qu.push(num);
+    }
+};
