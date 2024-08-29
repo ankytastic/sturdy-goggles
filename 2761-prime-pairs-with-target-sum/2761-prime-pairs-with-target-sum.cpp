110a45143025d729f8e889a19dea752f67ad409a
ANKIT RAJ <109132971+ankytastic@users.noreply.github.com>
2024-08-29 12:19:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2761-prime-pairs-with-target-sum/2761-prime-pairs-with-target-sum.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> S(int n) {
+        bool prime[n + 1];
+        memset(prime, true, sizeof(prime));
+        for (int p = 2; p * p <= n; p++) {
+            if (prime[p] == true) {
+                for (int i = p * p; i <= n; i += p)
+                    prime[i] = false;
+            }
+        }
+        vector<int> r;
+        for (int p = 2; p <= n; p++) {
+            if (prime[p]) {
+                r.push_back(p);
+            }
+        }
+        return r;
+    }
+    vector<vector<int>> findPrimePairs(int n) {
+        vector<vector<int>> res;
+        vector<int> primes=S(n);
+        if (n <= 2)
+            return res;
+        for(int i=0;i<primes.size();i++){
+            if(primes[i]>(n/2+1)) break;
+            int k=n-primes[i];
+            auto it=find(primes.begin(), primes.end(), k);
+            if(it!=primes.end()){
+                res.push_back({primes[i],k});
+                primes[it - primes.begin()]=-1;
+            }
+        }
+        return res;
+    }
+};
