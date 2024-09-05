2c78a223c3997cdee3addc8c8b2ea3f18095f661
ANKIT RAJ <109132971+ankytastic@users.noreply.github.com>
2024-09-05 21:44:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3274-check-if-two-chessboard-squares-have-the-same-color/3274-check-if-two-chessboard-squares-have-the-same-color.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool checkTwoChessboards(string coordinate1, string coordinate2) {
+        int c1=coordinate1[0]-'a';
+        int c2=coordinate2[0]-'a';
+        int r1=coordinate1[1]-'0';
+        int r2=coordinate2[1]-'0';
+        return (c1==c2?(r1%2)==(r2%2):((c1%2==c2%2)?((r1%2)==(r2%2)):false));
+    }
+};
