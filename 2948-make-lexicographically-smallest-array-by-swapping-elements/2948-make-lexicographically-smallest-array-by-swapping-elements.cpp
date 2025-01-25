8b5d21c58fac2cfe7d0b7fcada8c98cd0210bddd
ANKIT RAJ <109132971+ankytastic@users.noreply.github.com>
2025-01-25 21:57:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
+        int n = nums.size();
+
+        for(int i = 0; i < n; i++) {
+
+            while(true) {
+                int smallValue = nums[i];
+                int idx = -1;
+
+                for(int j = i+1; j < n; j++) {
+                    if(abs(nums[i] - nums[j]) <= limit) {
+                        if(nums[j] < smallValue) {
+                            smallValue = nums[j];
+                            idx = j;
+                        }
+                    }
+                }
+
+                if(idx != -1) {
+                    swap(nums[i], nums[idx]);
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return nums;
+    }
+};
