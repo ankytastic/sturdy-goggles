07e344bc80a2d067ae86172409d81a35763cdde1
ANKIT RAJ <109132971+ankytastic@users.noreply.github.com>
2025-08-01 12:34:43 +0530

    Time: 1 ms (99.49%), Space: 45.9 MB (8.29%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -3,13 +3,12 @@ class Solution {
         int j=0;
         int currEnd=0;
         int far=0;
-        for(int i=0;i<nums.length;i++){
+        for(int i=0;i<nums.length-1;i++){
             far=Math.max(far,i+nums[i]);
             if(i==currEnd){
                 j+=1;
                 currEnd=far;
             }
-            if(currEnd==nums.length-1) break;
         }
         return j;
     }
