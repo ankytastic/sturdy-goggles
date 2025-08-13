ed97e76450075b6029cdb25726bc58f28e898479
ANKIT RAJ <109132971+ankytastic@users.noreply.github.com>
2025-08-13 18:14:42 +0530

    Time: 3 ms (89.58%) | Memory: 23.8 MB (80.15%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& i) {
+        if (i.size() == 1)
+            return i;
+        // i.sort();
+        sort(i.begin(), i.end(),
+             [](const vector<int>& a, const vector<int>& b) {
+                 return a[0] < b[0];
+             });
+        vector<vector<int>> r;
+        vector<int> curr = i[0];
+        for (int j = 1; j < i.size(); j++) {
+            if (curr[1] >= i[j][0]) {
+                curr[1] = max(curr[1], i[j][1]);
+            } else {
+                r.push_back(curr);
+                curr = i[j];
+            }
+        }
+        r.push_back(curr);
+        return r;
+    }
+};
