92b3cc308abd0f2f28bd59e2c41935fee45b4963
ANKIT RAJ <109132971+ankytastic@users.noreply.github.com>
2025-08-21 16:58:46 +0530

    Time: 3 ms (44.08%) | Memory: 10.7 MB (8.89%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,24 +1,16 @@
-#include <unordered_map>
-
 class Solution {
 public:
-    bool isIsomorphic(string s, string t) {
-        unordered_map<char, char> s_to_t;
-        unordered_map<char, char> t_to_s;
-        for (int i = 0; i < s.size(); ++i) {
-            char s_char = s[i];
-            char t_char = t[i];
-            if (s_to_t.find(s_char) == s_to_t.end()) {
-                s_to_t[s_char] = t_char;
-            } else {
-                if (s_to_t[s_char] != t_char) return false;
-            }
-            if (t_to_s.find(t_char) == t_to_s.end()) {
-                t_to_s[t_char] = s_char;
-            } else {
-                if (t_to_s[t_char] != s_char) return false;
-            }
+    vector<int> charmap(string s){
+        vector<int> pos;
+        map<char,int> mpp;
+        for(int i=0;i<s.size();i++){
+            if(mpp.find(s[i])==mpp.end()) mpp[s[i]]=i;
+            pos.push_back(mpp[s[i]]);
         }
-        return true;
+        return pos;
+    }
+    bool isIsomorphic(string s, string t) {
+        if(s.size()!=t.size()) return false;
+        return charmap(s)==charmap(t);
     }
 };
