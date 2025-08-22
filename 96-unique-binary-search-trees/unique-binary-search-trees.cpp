80977df844485eb46aacc8cd451f63409c2aa286
ANKIT RAJ <109132971+ankytastic@users.noreply.github.com>
2025-08-22 18:56:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (16.87%) - LeetSync

---
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> dp(n+1);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            int t=0;
+            for(int j=1;j<=i;j++){
+                int l=dp[j-1];
+                int r=dp[i-j];
+                t+=l*r;
+            }
+            dp[i]=t;
+        }
+        return dp[n];
+    }
+};
