3c9f47941c5fcb53932174a751a3770daaaad7c3
ANKIT RAJ <109132971+ankytastic@users.noreply.github.com>
2026-03-03 23:21:12 +0530

    Time: 140 ms (6.56%), Space: 184 MB (19.21%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,35 +1,39 @@
 class LRUCache {
 public:
-    vector<pair<int, int>> lru;
-    int c;
-    LRUCache(int capacity) { c = capacity; }
-
-    int get(int key) {
-        if(lru.size()==0) return -1;
-        for (int i = 0; i < lru.size(); i++) {
-            if (lru[i].first == key) {
-                int k = lru[i].second;
-                lru.erase(lru.begin() + i);
-                lru.push_back({key, k});
-                return k;
-            }
-        }
-        return -1;
+    list<int> dll;
+    map<int,pair<list<int>::iterator,int>> mp;
+    int n;
+    LRUCache(int capacity) {
+        n=capacity;
+    }
+    
+    void makeMRU(int key){
+        dll.erase(mp[key].first);
+        dll.push_front(key);
+        mp[key].first=dll.begin();
     }
 
+    int get(int key) {
+        if(mp.find(key)==mp.end()) return -1;
+        makeMRU(key);
+        return mp[key].second;
+    }
+    
     void put(int key, int value) {
-        for (int i = 0; i < lru.size(); i++) {
-            if (lru[i].first == key) {
-                lru.erase(lru.begin() + i);
-                lru.push_back({key, value});
-                return;
-            }
+        if(mp.find(key)!=mp.end()){
+            mp[key].second=value;
+            makeMRU(key);
+        }else{
+            dll.push_front(key);
+            mp[key]={dll.begin(),value};
+            n--;
         }
-        if (lru.size() >= c) {
-            lru.erase(lru.begin());
+        if(n<0){
+            int d=dll.back();
+            mp.erase(d);
+            dll.pop_back();
+            n++;
         }
-        lru.push_back({key, value});
-        return;
     }
 };
 
