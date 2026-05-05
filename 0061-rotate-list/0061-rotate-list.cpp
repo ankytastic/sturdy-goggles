c723ee296045f284eeb6886b9f91430a4f753664
ANKIT RAJ <109132971+ankytastic@users.noreply.github.com>
2026-05-05 21:28:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -1,38 +1,23 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* find(ListNode* temp, int n){
-        int cnt=1;
-        while(temp!=NULL){
-            if(cnt==n) return temp;
-            cnt++;
-            temp=temp->next;
-        }
-        return temp;
-    }
     ListNode* rotateRight(ListNode* head, int k) {
-        if(head==NULL || k==0) return head;
-        ListNode* tail=head;
-        int len=1;
-        while(tail->next!=NULL){
-            tail=tail->next;
-            len++;
+        if(!head || !head->next || k == 0) return head;
+        int n = 1;
+        ListNode* tail = head;
+        while(tail->next){
+            tail = tail->next;
+            n++;
         }
-        if(k%len==0) return head;
-        k=k%len;
-        tail->next=head;
-        ListNode* newLastNode=find(head,len-k);
-        head=newLastNode->next;
-        newLastNode->next=NULL;
-        return head;
+        k = k % n;
+        if(k == 0) return head;
+        tail->next = head;
+        int steps = n - k;
+        ListNode* newTail = head;
+        for(int i = 1; i < steps; i++){
+            newTail = newTail->next;
+        }
+        ListNode* newHead = newTail->next;
+        newTail->next = NULL;
+        return newHead;
     }
 };
